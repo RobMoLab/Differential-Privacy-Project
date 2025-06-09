d2f5a2568a853146fdd785be89f24765a5370fd3
Ogonna11 <84002969+Ogonna11@users.noreply.github.com>
2025-06-09 15:11:54 -0500

    Update power_level_time_chisquare_over_n_NP.R

--- DP_Fiducial_Matching_Codes/DP_chi_square_test/power_level_time_chisquare_over_n_NP.R
+++ DP_Fiducial_Matching_Codes/DP_chi_square_test/power_level_time_chisquare_over_n_NP.R
@@ -48,57 +48,17 @@ Nonprivate_simulation <- function(joint_probs0, n_vec, alpha, sim) {
     Median_Time_ms = median_times
   )
   
-  # Create the combined plot
-  create_plot <- function() {
-    par(mar = c(5, 4, 4, 4) + 0.3)  # Extra space on right
-    
-    # Power plot (left axis)
-    plot(n_vec, results_df$Power, type = "b", pch = 16, col = "blue",
-         xlab = "Sample Size (n)", ylab = "Power",
-         main = "Power and Computation Time vs Sample Size",
-         ylim = c(0, 1))
-    
-    # Mean time (right axis)
-    par(new = TRUE)
-    plot(n_vec, mean_times, type = "b", pch = 17, col = "red",
-         axes = FALSE, xlab = "", ylab = "",
-         ylim = c(0, max(c(mean_times, median_times), na.rm = TRUE) * 1.1))
-    
-    # Median time (right axis)
-    par(new = TRUE)
-    plot(n_vec, median_times, type = "b", pch = 18, col = "green",
-         axes = FALSE, xlab = "", ylab = "",
-         ylim = c(0, max(c(mean_times, median_times), na.rm = TRUE) * 1.1))
-    
-    axis(side = 4)
-    mtext("Computation Time (ms)", side = 4, line = 3)
-    legend("topleft", legend = c("Power", "Mean Time", "Median Time"),
-           col = c("blue", "red", "green"), pch = c(16, 17, 18), lty = 1)
-  }
-  
-  # Only plot if we have valid data
-  if (all(is.finite(results_df$Power)) && any(is.finite(c(mean_times, median_times)))) {
-    create_plot()
-  } else {
-    warning("Insufficient valid data for plotting")
-  }
-  
-  return(list(
-    results = results_df,
-    time_matrix = time_matrix
-  ))
+  return(list(results = results_df, time_matrix = time_matrix))
 }
 
 # Level
 delta = 0.01
-joint_probs0 <-c(0.25, 0.25,0.25, 0.25) #+ delta*c(1,-1,-1,1) 
+joint_probs0 <-c(0.25, 0.25,0.25, 0.25) 
 n_vec <- c(20,30,50, 100, 200, 500, seq(1000, 20000, by = 1000))
-sim <- 10^3 # Reduced for demonstration
+sim <- 10^3 
 alpha <- 0.05
 
 level_time_chisquare_over_n_NP <- Nonprivate_simulation(joint_probs0, n_vec, alpha, sim)
-
-save(level_time_chisquare_over_n_NP, file = "level_time_chisquare_over_n_NP.Rda")
 
 print(level_time_chisquare_over_n_NP$results)
 
@@ -108,6 +68,5 @@ joint_probs0 <-c(0.25, 0.25,0.25, 0.25) + delta*c(1,-1,-1,1)
 
 power_time_chisquare_over_n_NP <- Nonprivate_simulation(joint_probs0, n_vec, alpha, sim)
 
-save(power_time_chisquare_over_n_NP, file = "power_time_chisquare_over_n_NP.Rda")
 
 print(power_time_chisquare_over_n_NP$results)
